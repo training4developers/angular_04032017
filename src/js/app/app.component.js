dcfd98610ab7607cba25b4965fd937e909e35635
Eric W. Greene <eric@ericwgreene.com>
2017-04-04 11:19:25 -0400

    added code demos

--- src/js/app/app.component.js
+++ src/js/app/app.component.js
@@ -2,8 +2,37 @@
 class AppComponent {
 
     header = 'Color Tool';
+    colors = ['red','blue','yellow'];
+    lastColors = null;
+    newColor = '';
 
+    addColor() {
+        this.colors = this.colors.concat(this.newColor);
+    }
+
+    get sortedColors() {
+
+        if (this.lastColors !== this.colors) {
+            console.log('sorting colors');
+            this.colors.sort();
+            this.lastColors = this.colors;
+        }
+
+        return this.colors;
+    }
 }
+
+// function AppComponent() {
+
+//     Object.defineProperty(this, 'sortedColors', {
+//         configurable: true,
+//         enumerable: true,
+//         get: function() {
+//             return this.colors;
+//         }
+//     });
+
+// }
 
 export const appComponent = [ 'main', {
     templateUrl: 'app/app.component.html',
--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -2,4 +2,10 @@
 import { appComponent } from './app.component';
 
 export const appModule = angular.module('App', [])
+    .filter('demo', function() {
+        return function(value) {
+            console.log('demo ran');
+            return value;
+        };
+    })
     .component(...appComponent);
