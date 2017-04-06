69e3f7f024080305a64fe81a99c742fb4aa2ce47
Eric W. Greene <eric@ericwgreene.com>
2017-04-06 13:14:20 -0400

    fixed unit tests with mock service for appStore and actions

--- src/js/app/app.component.spec.js
+++ src/js/app/app.component.spec.js
@@ -3,20 +3,42 @@ import 'angular-mocks';
 
 import { appModule } from './app.module';
 
+// the appStore and actions need mocks to test appComponent
+// the solution also demonstrates in general on approach to mocking
+// your own services, appMockModule could be moved its own file
+// so other unit tests could make use of it
+const appMockModule = angular.module('App.Mock', [])
+    .value('appStore', { 
+        getState() { },
+        subscribe() {
+            return () => {};
+        },
+        dispatch() { },
+    })
+    .value('actions', { 
+        refreshColors() { },
+        addColor() { },
+    });
+
 describe('app component tests', () => {
 
     let mockScope = null;
     let compileSvc = null;
 
-
     beforeEach(angular.mock.module(appModule.name));
+    beforeEach(angular.mock.module(appMockModule.name));
 
-    beforeEach(angular.mock.inject(['$rootScope', '$compile', ($rootScope, $compile) => {
+    beforeEach(angular.mock.inject([
+        '$rootScope', '$compile', 'appStore',
+        ($rootScope, $compile, appStore) => {
 
-        mockScope = $rootScope.$new(true);
-        compileSvc = $compile;
+            console.log(appStore.getState());
 
-    }]));
+            mockScope = $rootScope.$new(true);
+            compileSvc = $compile;
+
+        },
+    ]));
 
     it('dom structure', () => {
 
@@ -24,7 +46,6 @@ describe('app component tests', () => {
         const componentElement = linkingFn(mockScope);
 
         mockScope.$parent.$digest();
-
 
         expect(componentElement.find('h3').text(), 'Color Tool!');
     });
